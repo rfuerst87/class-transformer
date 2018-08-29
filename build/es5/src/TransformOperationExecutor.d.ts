d791f5fc25fa7ff7d2d4e5eb49a4515327e95b0f
Roman Fuerst <roman.fuerst@becompany.ch>
2018-08-29 16:19:48 +0200

    Include build.

---
+++ build/es5/src/TransformOperationExecutor.d.ts
@@ -0,0 +1,20 @@
+import { ClassTransformOptions } from "./ClassTransformOptions";
+import { TypeMetadata } from "./metadata/TypeMetadata";
+export declare enum TransformationType {
+    PLAIN_TO_CLASS = 0,
+    CLASS_TO_PLAIN = 1,
+    CLASS_TO_CLASS = 2,
+}
+export declare class TransformOperationExecutor {
+    private transformationType;
+    private options;
+    private transformedTypesMap;
+    constructor(transformationType: TransformationType, options: ClassTransformOptions);
+    transform(source: Object | Object[] | any, value: Object | Object[] | any, targetType: Function | TypeMetadata, arrayType: Function, isMap: boolean, level?: number): any;
+    private applyCustomTransformations(value, target, key, obj, transformationType);
+    private isCircular(object, level);
+    private getReflectedType(target, propertyName);
+    private getKeys(target, object);
+    private checkVersion(since, until);
+    private checkGroups(groups);
+}
